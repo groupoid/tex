2b8ffcbdcb14667002a1106d597db5fc9e382d43
Namdak Tonpa <maxim@synrc.com>
2026-03-23 01:23:59 +0200

    wip

--- Engine/Output.ml
+++ Engine/Output.ml
@@ -1,25 +1,23 @@
 
-open Tools.XNum
+open XNum
 open Runtime
 open Job
 
 let output_pages job pages =
   match pages with
-    [] -> ()
-  | _ ->
-      let comment =
-        " ant output " ^ string_of_int (job.time.Unix.tm_year + 1900) ^ "." ^
-        string_of_int (job.time.Unix.tm_mon + 1) ^ "." ^
-        string_of_int job.time.Unix.tm_mday ^ ":" ^
-        string_of_int job.time.Unix.tm_hour ^
-        string_of_int job.time.Unix.tm_min
-      in
-      match job.output_format with
-        DVI -> GenerateDVI.write_dvi_file job.output_file comment pages
-      | XDVI -> GenerateDVI.write_xdvi_file job.output_file comment pages
-      | PDF -> Runtime.GeneratePDF.write_pdf_file job.output_file comment pages
-      | PS ->
-          GeneratePostScript.write_postscript_file job.output_file comment
-            pages
-      | SVG -> GenerateSVG.write_svg_file job.output_file comment pages
+  | [] -> ()
+  | _  ->
+    let comment = " ant output "
+                ^ string_of_int (job.time.Unix.tm_year + 1900) ^ "."
+                ^ string_of_int (job.time.Unix.tm_mon + 1) ^ "."
+                ^ string_of_int job.time.Unix.tm_mday ^ ":"
+                ^ string_of_int job.time.Unix.tm_hour
+                ^ string_of_int job.time.Unix.tm_min in
+
+    match job.output_format with
+    | DVI  -> GenerateDVI.write_dvi_file               job.output_file comment pages
+    | XDVI -> GenerateDVI.write_xdvi_file              job.output_file comment pages
+    | PDF  -> GeneratePDF.write_pdf_file               job.output_file comment pages
+    | PS   -> GeneratePostScript.write_postscript_file job.output_file comment pages
+    | SVG  -> GenerateSVG.write_svg_file               job.output_file comment pages
 
---
+++ Runtime/FontFT.mli
@@ -0,0 +1,6 @@
+
+open Tools.XNum
+open FontMetric
+
+val read_ft : string -> string -> font_load_params -> font_metric
+
